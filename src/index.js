bd6900170bfc761c24a7d4c5bc1c239c6d3f40ab
grshkvch <volody_200115@mail.ru>
2022-02-24 22:54:28 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let encodedLetters = []  //array of encoded letters with length = 10
+
+    for (let i = 0; i < expr.length; i += 10) {
+        encodedLetters.push(expr.slice(i, (i + 10)))
+    }
+
+    let dashesAndDotes = encodedLetters.map((encode) => {   //array of dashes and dotes
+        if (encode == '**********') {
+            return ' '
+        }
+        let unencoded = ''
+        for (i = 0; i < 10; i += 2) {
+            if (encode[i] == '1' && encode[i + 1] == '1') {
+                unencoded += '-'
+            } else if (encode[i] == '1' && encode[i + 1] == '0') {
+                unencoded += '.'
+            }
+        }
+        return unencoded
+    })
+
+    let unencodedLetters = dashesAndDotes.map((unencode) => {     //array of unencoded letters 
+        if (unencode === ' ') {
+            return ' '
+        } else {
+            return MORSE_TABLE[unencode]
+        }
+    });
+
+    let final = unencodedLetters.reduce((acc, letter) => {      //final string 
+        acc += letter;
+        return acc
+    }, '')
+
+    return final 
 }
 
 module.exports = {
